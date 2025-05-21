0e52fdb9fc50740bc35fed0c7842d3f597c953b2
Роман <129206895+Resenkov@users.noreply.github.com>
2025-05-21 18:51:12 +0300

    feat: добавил репозитории.

---
+++ src/main/java/resenkov/work/task1t1/repository/DataSourceErrorLogRepository.java
@@ -0,0 +1,7 @@
+package resenkov.work.task1t1.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import resenkov.work.task1t1.entity.DataSourceErrorLog;
+
+public interface DataSourceErrorLogRepository extends JpaRepository<DataSourceErrorLog, Long> {
+}
