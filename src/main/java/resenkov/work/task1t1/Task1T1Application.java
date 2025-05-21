131e937209db4b343d7b58a9415fd50e38c99caf
Роман <129206895+Resenkov@users.noreply.github.com>
2025-05-21 18:29:45 +0300

    Initial commit

---
+++ src/main/java/resenkov/work/task1t1/Task1T1Application.java
@@ -0,0 +1,13 @@
+package resenkov.work.task1t1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task1T1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Task1T1Application.class, args);
+    }
+
+}
---
+++ src/test/java/resenkov/work/task1t1/Task1T1ApplicationTests.java
@@ -0,0 +1,13 @@
+package resenkov.work.task1t1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task1T1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
